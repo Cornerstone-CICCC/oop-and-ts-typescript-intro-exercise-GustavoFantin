43bd9e9e81f409ed72de4ffc1413c9a6efe14548
GustavoFantin <gustavofantinbarros@gmail.com>
2025-02-04 12:03:35 -0800

    exercise 1-5 done

--- dist/ex02.js
+++ dist/ex02.js
@@ -1,20 +1,19 @@
+"use strict";
 // 2. Create a function named `isPalindrome` that takes a string as a parameter.
 //    The function should return `true` if the string is a palindrome (reads the same backward as forward),
 //    otherwise, return `false`.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function isPalindrome(word: string): boolean {
-    const turnArr = word.split('')
+function isPalindrome(word) {
+    const turnArr = word.split('');
     const reverse = turnArr.reverse();
-    const getReversed = reverse.join('')
-    
+    const getReversed = reverse.join('');
     if (word === getReversed) {
-        return true
-    } else {
-        return false
+        return true;
+    }
+    else {
+        return false;
     }
 }
-
 // Expected output:
 console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("hello"));   // false
+console.log(isPalindrome("hello")); // false
